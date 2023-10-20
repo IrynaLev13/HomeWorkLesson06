7ded434f1a27c7529940a7473ee975319b8bd76c
IrynaLev13 <ikot@ukr.net>
2023-10-20 16:02:02 +0300

    HomeWork13MatrixCommit2

--- src/HW13Matrix.java
+++ src/HW13Matrix.java
@@ -11,6 +11,7 @@ public class HW13Matrix {
         System.out.println("Enter value for N:");
        int n = scanner.nextInt();
         int[][] array012D = new int[m][n];
+        int [][] newmatrix = new int[n][m];
         for (int i = 0; i < array012D.length; i++) {
             for (int j = 0; j < array012D[i].length; j++) {
 
@@ -24,16 +25,16 @@ public class HW13Matrix {
             System.out.println();
 
         }
-        int[][] array022D = new int[n][m];
-        for (int i = 0; i < array022D.length; i++) {
-            for (int j = 0; j < array022D[i].length; j++) {
+        System.out.println("New matrix");
+         for (int i = 0; i < array012D.length; i++) {
+            for (int j = 0; j < array012D[i].length; j++) {
+                newmatrix  [j][i] = array012D [i][j];
 
-                array022D[i][j] = random.nextInt(10);
             }
         }
-        for (int i = 0; i < array022D.length; i++) {
-            for (int j = 0; j < array022D[i].length; j++) {
-                System.out.print(array022D[i][j] + ", ");
+        for (int i = 0; i < newmatrix.length; i++) {
+            for (int j = 0; j < newmatrix [i].length; j++) {
+                System.out.printf(newmatrix[i][j] + ", ");
             }
             System.out.println();
         }
